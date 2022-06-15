88b409ae3598f3a4e40d3df66494281e5d581515
sagarrpatil <66763299+sagarrpatil@users.noreply.github.com>
2022-06-15 15:46:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -37,10 +37,11 @@ setInterval(() => {
     "reqDate": ""
   }
  if((moment().format("dddd")!=="Saturday" || moment().format("dddd")!=="Sunday")  
- &&(moment().format("a")=="am" && Number(moment().format("hh")>=9) ||
-  (moment().format("a")=="pm" &&
- ((Number(moment().format("hh"))<=3 && (Number(moment().format("hh"))==3 && Number(moment().format("mm"))<31)) || Number(moment().format("hh"))==12 || Number(moment().format("hh"))<=2))
- )){
+      &&(moment().format("a")=="am" && 
+          (Number(moment().format("hh")>=10) || (Number(moment().format("hh"))==9 && Number(moment().format("mm")>14))) ||
+        (moment().format("a")=="pm" &&
+          ((Number(moment().format("hh"))<=3 && (Number(moment().format("hh"))==3 && Number(moment().format("mm"))<31)) || Number(moment().format("hh"))==12 || Number(moment().format("hh"))<=2))
+      )){
     console.log("open")
     axios.post("https://api.niftytrader.in/webapi/Option/getOiPcrListData", niftypcrdata).then((response)=>{
       let oiDataMonthPCRNifty=response.data.resultData.oiDatas;
