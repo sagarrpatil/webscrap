9cf3db8b1704fc18d2137b203dbdeff14e25912d
sagar <sagarrpatil9607@gmail.com>
2023-09-16 14:22:57 +0530

    testing

--- index.js
+++ index.js
@@ -283,8 +283,16 @@ const getValueFromNSE = async (symbol) =>{
   try{
     let configHeader = {
       headers: {
-        // "Referer" : "https://www.nseindia.com/report-detail/eq_security",
-        "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+        'Host':'www.nseindia.com', 
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 
+        'Accept-Language':'en-US,en;q=0.5', 
+        'Accept-Encoding':'gzip, deflate, br',
+        'DNT':'1', 
+        'Connection':'keep-alive', 
+        'Upgrade-Insecure-Requests':'1',
+        'Pragma':'no-cache',
+        'Cache-Control':'no-cache', 
       }
     }
     console.log("test")
