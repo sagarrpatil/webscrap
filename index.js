c67c2bb25c4945bd6d08fce7f5236a46d6470348
sagar <sagarrpatil9607@gmail.com>
2023-10-06 15:08:21 +0530

    changes of pcr

--- index.js
+++ index.js
@@ -306,68 +306,91 @@ database.ref(`/`).on('value', async (snapshot) => {
       console.log(error);
     }
   }
-  async function scrapeGrowwData() {
-    try {
-      const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  const valuebrowser = async () => {
+    const currentDate = moment();
+    const daysUntilThursday = (4 - currentDate.day() + 7) % 7;
+    const nextThursday = currentDate.add(daysUntilThursday, 'days');
+    const browser = await puppeteer.launch({ headless: 'new',} );
+    const page = await browser.newPage();
+    let exp = nextThursday.format("YYYY-MM-DD");
+    const url = `https://www.moneycontrol.com/indices/fno/view-option-chain/NIFTY/${exp}`;
+    const headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Connection': 'keep-alive',
+      'Upgrade-Insecure-Requests': '1',
+      'Cache-Control': 'max-age=0',
+    };
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'); // Optional
+
+    // Set custom headers
+    await page.setExtraHTTPHeaders(headers); // Optional
+
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    console.log(page)
+    // Extract the data using Puppeteer
+    const jsonData = await page.evaluate(() => {
+      const customKeys = [
+        'callOI',
+        'callChangeOI',
+        'callVolume',
+        'callChangeLTP',
+        'callLTP',
+        'strike',
+        'putLTP',
+        'putChangeLTP',
+        'putVolume',
+        'putChangeOI',
+        'putOI',
+      ];
+      const rows = Array.from(document.querySelectorAll('tbody tr'));
+
+      return rows.map((row) => {
+        const columns = Array.from(row.querySelectorAll('td'));
+        const rowData = {};
+
+        columns.forEach((column, index) => {
+          const columnName = document.querySelectorAll('thead th')[index].textContent.trim();
+          const cellValue = column.textContent.trim();
+          rowData[customKeys[index]] = cellValue;
+        });
+
+        return rowData;
       });
-  
-      const currentDate = moment();
-      const daysUntilThursday = (4 - currentDate.day() + 7) % 7;
-      const nextThursday = currentDate.add(daysUntilThursday, 'days');
-      const exp = nextThursday.format("YYYY-MM-DD");
-      console.log(exp)
-      const page = await browser.newPage();
-      const userAgent =
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36';
-      await page.setUserAgent(userAgent);
-      await page.goto('https://groww.in/options/nifty?expiry=' + exp, { timeout: 60000 });
-      await page.waitForTimeout(10000);
-  
-      const currentValue = "..."; 
-    
-  
-  
-      const callResponse = await axios.post("https://groww.in/v1/api/stocks_fo_data/v1/tr_live_prices/exchange/NSE/segment/FNO/latest_prices_batch", call, headers);
-      const putResponse = await axios.post("https://groww.in/v1/api/stocks_fo_data/v1/tr_live_prices/exchange/NSE/segment/FNO/latest_prices_batch", put, headers);
-      const { sumofCallChangeOI, sumofPutChangeOI } = calculateChangeOI(callResponse.data);
-      const { sumofCallChangeOIPE, sumofPutChangeOIPE } = calculateChangeOI(putResponse.data);
-  
-      const ceCall = sumofCallChangeOI + sumofCallChangeOIPE;
-      const pePut = sumofPutChangeOI + sumofPutChangeOIPE;
-  
-      const data = {
-        sumofCallChangeOI: ceCall,
-        sumofPutChangeOI: pePut,
-        currentValue: currentValue,
-        changeInPCR: Number(pePut / ceCall).toFixed(2),
-      };
-console.log(callResponse)
-      if (sumofCallChangeOI) {
-        console.log(data);
-        database.ref(`/niftyChangeOI/`).set(data);
+    });
+    const { sumofCallChangeOI, sumofPutChangeOI } = jsonData.reduce(
+      (acc, val) => {
+        const callValChangeOI = Number(val.callChangeOI?.replace(/,/g, '')) || 0;
+        const putValChangeOI = Number(val.putChangeOI?.replace(/,/g, '')) || 0;
+
+        if (callValChangeOI > 0) {
+          acc.sumofCallChangeOI += callValChangeOI;
+        } else {
+          acc.sumofPutChangeOI += -callValChangeOI;
+        }
+
+        if (putValChangeOI > 0) {
+          acc.sumofPutChangeOI += putValChangeOI;
+        } else {
+          acc.sumofCallChangeOI += -putValChangeOI;
+        }
+        return acc;
+      },
+      { sumofCallChangeOI: 0, sumofPutChangeOI: 0 } );
+      const changeInPCR = Number(sumofPutChangeOI / sumofCallChangeOI).toFixed(2);
+      console.log("pcr",changeInPCR)
+
+      const  PCR = Number(sumofPutChangeOI / sumofCallChangeOI).toFixed(2);
+      if (sumofCallChangeOI && PCR) {
+        database.ref(`/niftyChangeOI/sumofCallChangeOI`).set(sumofCallChangeOI);
+        database.ref(`/niftyChangeOI/sumofPutChangeOI`).set(sumofPutChangeOI);
+        database.ref(`/niftyChangeOI/changeInPCR`).set(PCR);
       }
-  
-      await browser.close();
-    } catch (error) {
-      console.error("Error:", error);
-    }
+    await browser.close();
   }
-  
-  function calculateChangeOI(data) {
-    return Object.values(data).reduce((accumulator, current) => {
-      const callValChangeOI = parseFloat(current.oiDayChange) || 0;
-  
-      if (callValChangeOI > 0) {
-        accumulator.sumofCallChangeOI += callValChangeOI;
-      } else {
-        accumulator.sumofPutChangeOI += -callValChangeOI;
-      }
-  
-      return accumulator;
-    }, { sumofCallChangeOI: 0, sumofPutChangeOI: 0 });
-  }
+ 
   
   async function fetchMoneyControlData() {
     try {
@@ -402,9 +425,7 @@ console.log(callResponse)
   
   // Schedule tasks
   cron.schedule('* 9-16 * * 1-5', async () => {
-    await scrapeGrowwData();
+    await valuebrowser();
+    setInterval(fetchMoneyControlData, 6000);
+    setInterval(fetchOptionChainData, 10000);
   });
-  
-  setInterval(fetchMoneyControlData, 6000);
-  setInterval(fetchOptionChainData, 10000);
-  
